eff203a2a5761a8efb7fe1c7342eab39e6f56da2
Vishal Bhardwaj <infovishalbhardwaj232001@gmail.com>
2021-10-25 23:01:17 +0530

    check 2

---
+++ Vishal/src/Stacks/StackCheck.java
@@ -0,0 +1,13 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class StackCheck {
+
+	public static void main(String[] args) {
+		Stack<Integer> s=new Stack<>();
+		s.push(1);
+		System.out.println(s);
+		
+	}
+}
